d059a75819430be08296c32c6b29917a7bb2fb90
AtonCode <49330231+AtonCode@users.noreply.github.com>
2021-09-01 03:47:07 -0500

    Mejora de Estructura

---
+++ UnnU/screens/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+const UserRegister = () => {
+    return(
+        <View>
+            <Text>Hello UserRegister</Text>
+        </View>
+    )
+}
+export default UserRegister
