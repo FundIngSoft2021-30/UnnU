877dcecad3de6fc3a59f7e2682b956aa4280e500
juan Pablo Sanchez Gaitan <Juansanchezg@javeriana.edu.co>
2021-09-27 23:51:02 -0500

    mongodb init

--- UnnU/ios/UnnU/AppDelegate.h
+++ UnnU/ios/UnnU/AppDelegate.h
@@ -3,12 +3,6 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@property(nonatomic, strong) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
 
 @end
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
-{
-    return [RCTLinkingManager application:app openURL:url options:options];
-}
