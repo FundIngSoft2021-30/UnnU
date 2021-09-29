e8d02090dea525a0a3408e93d9f6738909b95929
juan Pablo Sanchez Gaitan <Juansanchezg@javeriana.edu.co>
2021-09-29 01:14:07 -0500

    react js

--- UnnU/dataBase/mongoDB.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://AtonCode:<Chaman99+>@moonmicotechnologies.x8mjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
