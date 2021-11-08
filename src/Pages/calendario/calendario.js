7276992e5e8f762962d57f0047746f016f64e250
Nicolas <Vargas>
2021-11-08 17:21:38 -0500

    ahora con calendario

--- src/Pages/calendario/calendario.js
+++ src/Pages/calendario/calendario.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import CalendarSC from './components/calendariosc';
+
+import "./components/calendariosc.css";
+import { useState } from "react";
+import Calendar, { CalendarDayHeader } from "./components/calendariosc";
 import NavbarU from '../NavBarUser/NavBarUC';
 
-function Calendar() {
-    return (
-        <>
-            <NavbarU />
-            <CalendarSC />
-        </>
-    );
+export default function App() {
+  const [yearAndMonth, setYearAndMonth] = useState([2021, 9]);
+  return (
+    <div className="App">
+     <NavbarU />
+      <Calendar
+        yearAndMonth={yearAndMonth}
+        onYearAndMonthChange={setYearAndMonth}
+        renderDay={(calendarDayObject) => (
+          <div>
+            <CalendarDayHeader calendarDayObject={calendarDayObject} />
+          </div>
+        )}
+      />
+    </div>
+  );
 }
-
-export default Calendar;
