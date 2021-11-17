8dbb7566f270d97b7f40d863a1fcd97d21ace0d4
AtonCode <49330231+AtonCode@users.noreply.github.com>
2021-11-17 10:56:55 -0500

    tests

---
+++ src/__tests__/Calendario.test.js
@@ -0,0 +1,7 @@
+import { render,screen } from "@testing-library/react";
+import App from './App';
+import evetsCalendar from "../Pages/calendario/components/event-utils";
+
+it("Functional Test AddEventInCalendar",()=>{
+
+})
