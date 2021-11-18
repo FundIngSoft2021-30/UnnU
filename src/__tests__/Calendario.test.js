f04ee20ddf9cf55dd67d34a2d02fb437c7711fd6
AtonCode <49330231+AtonCode@users.noreply.github.com>
2021-11-18 00:54:36 -0500

    test

--- src/__tests__/Calendario.test.js
+++
@@ -1,7 +0,0 @@
-import { render,screen } from "@testing-library/react";
-import App from './App';
-import evetsCalendar from "../Pages/calendario/components/event-utils";
-
-it("Functional Test AddEventInCalendar",()=>{
-
-})
---
+++ src/__tests__/SingUp.test.js
@@ -0,0 +1,8 @@
+import { React } from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render } from "@testing-library/react";
+import SignUp from "../Pages/SignUp/components/SignUp";
+
+it("", ()=>{
+    
+})
